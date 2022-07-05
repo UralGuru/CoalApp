4150192acc4cf8a3978141a5d2013e19fc699eb1
UralGuru <gizzatullinu@gmail.com>
2022-07-05 13:56:44 +0500

    'Formik/Form/Field/ErrorMessage'

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useFormik} from "formik";
+import {Formik, Form, Field, ErrorMessage} from "formik";
 import * as yup from 'yup';
 import s from './login.module.css'
 
@@ -14,19 +14,6 @@ const LoginForm = () => {
         console.log(values);
     };
 
-    let validate = (values) => {
-        let errors = {}
-
-        if (!values.login) {
-            errors.login = 'Required'
-        }
-        if (!values.password) {
-            errors.password = 'Required'
-        }
-
-        return errors
-    };
-
     const validationSchema = yup.object({
         login: yup.string()
                         .email('Invalid email format')
@@ -34,46 +21,39 @@ const LoginForm = () => {
         password: yup.string().required('Required'),
     });
 
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema,
-        //validate
-    });
-
     //console.log('Formik errors ', formik.errors)
     //console.log('Formik touched ', formik.touched)
 
-    return <form onSubmit={formik.handleSubmit}>
-        <div className={s.formControl}>
-            <input placeholder={"Login"}
-                   name='login'
-                   type={"email"}
-                   {...formik.getFieldProps('login')}
-                   // onBlur={formik.handleBlur}
-                   // onChange={formik.handleChange}
-                   // value={formik.values.login}
-            />
-        </div>
-        {formik.touched.login && formik.errors.login ? <div className={s.error}> {formik.errors.login} </div> : null}
+    return <Formik initialValues={initialValues}
+                   validationSchema={validationSchema}
+                   onSubmit={onSubmit}>
+        <Form>
+            <div className={s.formControl}>
+                <Field placeholder={"Login"}
+                       name='login'
+                       type={"email"}
+                />
+            </div>
+            <ErrorMessage name={'login'} />
 
-        <div className={s.formControl}>
-            <input placeholder={"Password"}
-                   name='password'
-                   type={"password"}
-                   {...formik.getFieldProps('password')}
-            />
-        </div>
-        {formik.touched.password && formik.errors.password ? <div className={s.error}> {formik.errors.password} </div> : null}
+            <div className={s.formControl}>
+                <Field placeholder={"Password"}
+                       name='password'
+                       type={"password"}
+                />
+            </div>
+            <ErrorMessage name={'password'} />
 
-        <div>
-            <input type={"checkbox"}/>remember me
-        </div>
+            <div>
+                <input type={"checkbox"}/>remember me
+            </div>
 
-        <div>
-            <button type={"submit"}>LogIn</button>
-        </div>
-    </form>
+            <div>
+                <button type={"submit"}>LogIn</button>
+            </div>
+        </Form>
+    </Formik>
+
 }
 
 const Login = (props) => {
