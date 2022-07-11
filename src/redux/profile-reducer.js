b914bf8e103e79f8433519832019890b705a5712
UralGuru <gizzatullinu@gmail.com>
2022-07-11 17:19:03 +0500

    Test/createAction-DeletePost

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -4,12 +4,13 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
 
 let initialState = {
     posts: [
-        {message: 'Hi, how are you?', like_count: '5'},
-        {message: 'It\'s my first post', like_count: '11'},
-        {message: 'Hi', like_count: '55'}],
+        {postID: 1, message: 'Hi, how are you?', like_count: '5'},
+        {postID: 2, message: 'It\'s my first post', like_count: '11'},
+        {postID: 3, message: 'Hi', like_count: '55'}],
     newPostText: "it-camasutra it's best",
     profile: null,
     status: "",
@@ -20,7 +21,7 @@ const profileReducer = (state=initialState, action) => {
     switch (action.type) {
         case ADD_POST:{
             let newPost = {
-                id: 5,
+                postID: 5,
                 message: state.newPostText,
                 like_count: 0
             };
@@ -30,7 +31,6 @@ const profileReducer = (state=initialState, action) => {
                 newPostText: ""
             };
         }
-
         case UPDATE_NEW_POST_TEXT:{
             return {
                 ...state,
@@ -43,11 +43,12 @@ const profileReducer = (state=initialState, action) => {
                 status: action.status,
             };
         }
-
         case SET_USER_PROFILE: {
             return { ...state, profile: action.profile}
         }
-
+        case DELETE_POST: {
+            return { ...state, posts: state.posts.filter(p => p.postID != action.postID)}
+        }
         default:
             return state;
     }
@@ -55,10 +56,10 @@ const profileReducer = (state=initialState, action) => {
 };
 
 
-export const addPostActionCreator = () => ( {type: ADD_POST} );
+export const addPostActionCreator = () =>({ type: ADD_POST});
 export const setUserProfile = (profile) => ( {type: SET_USER_PROFILE, profile} );
 export const setStatys = (status) => ( {type: SET_STATUS, status} );
-
+export const deletePost = (postID) => ( {type:  DELETE_POST, postID} );
 export const getUserProfile = (userId) => {
     return (dispatch) => {
         userAPI.getProfile(userId)
@@ -75,7 +76,6 @@ export const getStatus = (userId) => {
             });
     }
 }
-
 export const updateStatus = (status) => {
     return (dispatch) => {
         profileAPI.updateStatus(status)
@@ -87,7 +87,6 @@ export const updateStatus = (status) => {
             });
     }
 }
-
 export const updateNewPostTextActionCreator = (text) => {
     return {
         type: UPDATE_NEW_POST_TEXT,
--- src/redux/tests/profile-reducer.test.js
+++ src/redux/tests/profile-reducer.test.js
@@ -1,22 +1,29 @@
-import profileReducer, {addPostActionCreator} from "../profile-reducer";
+import profileReducer, {addPostActionCreator, deletePost, updateNewPostTextActionCreator} from "../profile-reducer";
 import { render, screen } from '@testing-library/react';
 
+let state = {
+    posts: [
+        {postID: 1, message: 'Hi, how are you?', like_count: '5'},
+        {postID: 2, message: 'It\'s my first post', like_count: '11'},
+        {postID: 3, message: 'Hi', like_count: '55'}]
+};
 
 test('lenght of post should be incremented', () => {
     // 1. test data
     let action = addPostActionCreator("T E S T");
-    let state = {
-        posts: [
-            {message: 'Hi, how are you?', like_count: '5'},
-            {message: 'It\'s my first post', like_count: '11'},
-            {message: 'Hi', like_count: '55'}]
-    };
-
     // 2. action
     let newState = profileReducer(state, action);
-
     // 3. expection
     expect(newState.posts.length).toBe(4);//newState.posts.length === 4;
+});
+
+test('after deleting length of message should be decrement', () => {
+    // 1. test data
+    let action = deletePost(2);
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expection
+    expect(newState.posts.length).toBe(2);
 });
 
 
